a33bfacbcb57b163fc1b86d86f4e6624062eabbd
MdRashid838 <160717010+MdRashid838@users.noreply.github.com>
2024-05-15 17:42:29 +0530

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ setInterval(() => {
     d = new Date();
     htime = d.getHours();
     mtime = d.getMinutes();
-    stime = d.getSecond();
+    stime = d.getSeconds();
     
     hrotation = 30*htime + mtime/2;
     mrotation = 6*mtime;
